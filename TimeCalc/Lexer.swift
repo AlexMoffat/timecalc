c9ab230aa4a2dfa43fb1da39ed3023c9dd92dce5
amoffat <amoffat@spredfast.com>
2017-10-15 19:15:28 -0500

    Added license file.

--- TimeCalc/Lexer.swift
+++ TimeCalc/Lexer.swift
@@ -1,11 +1,32 @@
-//
-//  Lexer.swift
-//  TextViewTests
-//
-//  Created by Alex Moffat on 7/3/17.
-//  Copyright © 2017 Zanthan. All rights reserved.
-//
-// Format patterns defined http://www.unicode.org/reports/tr35/tr35-31/tr35-dates.html#Date_Format_Patterns
+/*
+ * Copyright (c) 2017 Alex Moffat
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of mosquitto nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 import Foundation
 
@@ -61,6 +82,7 @@ let formatterForTimeZone = {(zone: TimeZone?, format: String) -> DateFormatter i
 
 // Map from a regular expression to the generator to try if the expression matches. The first matching expression
 // with a generator that returns a token is used.
+// Format patterns defined http://www.unicode.org/reports/tr35/tr35-31/tr35-dates.html#Date_Format_Patterns
 let tokenGenerators: [(NSRegularExpression, TokenGenerator)] = {() -> [(NSRegularExpression, TokenGenerator)] in
     
     let match = {(r: NSTextCheckingResult, s: String) -> String in (s as NSString).substring(with: r.range(at: 1))}
